031f94439ad4e43c8821b8883efb8c8ce243a39a
Bryanet59 <123341722+Bryanet59@users.noreply.github.com>
2023-07-19 22:49:34 +0800

    Time: 2845 ms (17.76%), Space: 14.2 MB (76.78%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if(i != j and nums[i] + nums[j] == target):
+                    return [i,j]
+
+        return []
